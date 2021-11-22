bbbdbba299b1c1c7560ffc7711742d4a43d0fed1
Martin van Diest <martin.van.diest@intemo.com>
2021-11-22 12:56:47 +0100

    First

--- api2/settings.py
+++ api2/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'api2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': []
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,5 @@ from .views import BookListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
